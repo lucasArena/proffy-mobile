2282cff51a27caec32e4e1ab1067992cc809eade
Lucas Arena <lucasarenasantos@gmail.com>
2020-08-12 21:50:36 -0300

    Initial commit

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+const Landing: React.FC = () => {
+  return <Container />;
+};
+
+export default Landing;
